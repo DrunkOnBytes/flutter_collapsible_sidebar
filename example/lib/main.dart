4a6fd5a625427e6c3e1f0ce88ef32207b58d8467
RyuuKenshi <arjunsinha2122000@gmail.com>
2023-08-27 23:38:25 +0530

    dart 3 migration

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -260,7 +260,7 @@ class _SidebarPageState extends State<SidebarPage> {
             offset: Offset(-size.height * 0.3, -size.width * 0.23),
             child: Text(
               _headline,
-              style: Theme.of(context).textTheme.headline1,
+              style: Theme.of(context).textTheme.displayLarge,
               overflow: TextOverflow.visible,
               softWrap: false,
             ),
